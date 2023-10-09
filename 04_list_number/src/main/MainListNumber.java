fd9a53b2c62442596abb93cec72957b47dcd99ae
tarde <tarde@A13E15>
2023-10-09 20:09:17 +0200

    Sobreescritura de metodos

---
+++ 04_list_number/src/main/MainListNumber.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.ArrayList; 
+
+import list_number.ListNumber;
+
+public class MainListNumber {
+
+	public static void main(String[] args) {
+		
+		ListNumber miArray = new ListNumber();
+		
+		miArray.add(6);
+//		miArray.add(6);
+		
+		System.out.println(miArray.indexOf(0));
+		
+	}
+	
+}
---
+++ 05_utilizar_lastNumber/src/lolo/Prueba.java
@@ -0,0 +1,14 @@
+package lolo;
+
+import list_number.ListNumber;
+
+public class Prueba{
+
+	public static void main(String[] args) {
+		
+		ListNumber miLista = new ListNumber();
+		
+
+	}
+
+}
