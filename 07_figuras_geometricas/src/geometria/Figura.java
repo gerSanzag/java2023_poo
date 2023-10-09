a4729d3077e61c300f6a69fef5ae363997ae57ba
tarde <tarde@A13E15>
2023-10-09 20:52:26 +0200

    safdsa

---
+++ 07_figuras_geometricas/src/geometria/Figura.java
@@ -0,0 +1,20 @@
+package geometria;
+
+public class Figura {
+	
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Figura(String color) {
+		super();
+		this.color = color;
+	}
+	
+}
